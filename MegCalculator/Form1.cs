a29c1dcc578d274c34bf3c2a45e168e8e4e8e845
Sicelo Lubisi <sicelomeg@gmail.com>
2021-04-16 21:03:39 +0200

    First Commit

---
+++ MegCalculator/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegCalculator
+{
+    public partial class megCalculator : Form
+    {
+        public megCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = txtResult.Text + "1"; 
+        }
+    }
+}
